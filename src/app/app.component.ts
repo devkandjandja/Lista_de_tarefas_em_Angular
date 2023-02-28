5f75d1046a750b86d75039eab0a42eda0d1bce8e
devkandjadja <kandjandja94@gmail.com>
2023-02-28 12:35:52 +0100

    Primeiro commit do fornt end

---
+++ src/app/app.component.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public mode = 'list';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas tarefas';
+  public form: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });  
+    
+    this.load();
+  }
+
+  add()
+  {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1; 
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear()
+  {
+    this.form.reset();
+  }
+
+  remove(todo: Todo)
+  {
+    const index = this.todos.indexOf(todo);
+    if(index !== -1)
+    {
+      this.todos.splice(index, 1);
+    }
+    this.save();
+  }
+
+  markAsDone(todo: Todo)
+  {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUnDone(todo: Todo)
+  {
+   todo.done = false;
+   this.save();
+  }
+
+  save()
+  {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.mode = 'list';
+  } 
+  load()
+  {
+    const data = localStorage.getItem('todos');
+    if(data)
+    {
+      this.todos = JSON.parse(data);
+    } else 
+    {
+      this.todos = [];
+    }
+  }
+  changeMode(mode: string)
+  {
+    this.mode = mode;
+  }
+}
+
---
+++ src/models/todo.model.ts
@@ -0,0 +1,7 @@
+export class Todo {    
+    constructor(
+        public id: number,
+        public titulo: string, 
+        public done: boolean,       
+        )  {}
+}
